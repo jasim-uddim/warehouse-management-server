6477d93438d0ae01fbf8a4160f6aa3c3e07798f0
jasimuddin <jasimuddin25635@gmail.com>
2022-05-06 16:55:12 +0600

    repleas post

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // middleware
 app.use(cors());
